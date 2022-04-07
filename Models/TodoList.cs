7bc4d2f214b869b48968990521ecf0d84e4e9deb
AyandaMoreBoxfusion <AyandaM@boxfusion.co.za>
2022-04-07 15:15:06 +0200

    initial commit

---
+++ Models/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoListAPI.Models
+{
+	public class TodoList
+	{
+		public string Id { get; set; }
+		public string TaskName { get; set; }
+		public bool IsCompleted { get; set; }
+		public bool IsPriority { get; set; }
+		
+	}
+}
+
